acc4e0a0cff6c5046859f5b9a93223b442999685
Ojas Patil <161685651+ojasptl@users.noreply.github.com>
2024-11-21 07:02:19 +0000

    processCreation

--- processCreation.c
+++ processCreation.c
@@ -4,6 +4,10 @@
 #include<stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 
 int arr[100];
 int n;
@@ -12,11 +16,61 @@ void ascending (int p, int n) {
     int maxim = i;
     for (int j = 0; j < n; j++) {
       if(arr[i] > arr[j])
-        maxim = i;
+        maxim = j;
 
     }
+    int temp = arr[maxim];
+    arr[maxim] = arr[i];
+    arr[i] = temp;
   }
   for (int i = 0; i < n ; i++) {
-    printf("")
+    sleep(1);
+    printf("\t\t\t\ti=%d , p=%d , pid=%d , ppid=%d \n", arr[i], p, getpid(), getppid());
   }
 }
+
+void descending (int p, int n) {
+  for(int i = 0; i < n - 1; i++) {
+    int maxim = i;
+    for (int j = 0; j < n; j++) {
+      if(arr[i] < arr[j])
+        maxim = j;
+
+    }
+    int temp = arr[maxim];
+    arr[maxim] = arr[i];
+    arr[i] = temp;
+  }
+  for (int i = 0; i < n ; i++) {
+    sleep(10);
+    printf("i=%d,p=%d,pid=%d,ppid=%d\n", arr[i], p, getpid(), getppid());
+
+  }
+}
+
+void main() {
+  printf("Enter the no of processes\n");
+  int processes;
+  scanf("%d", &processes);
+  n = processes;
+  for (int i = 0; i < processes; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  int p = fork();
+  if (p == 0)
+  {
+    ascending(p, n);
+    exit(p);
+  }
+  else if (p > 0) {
+    descending(p, n);
+    exit(p);
+  }
+  else {
+    printf("failed");
+    exit(p);
+  }
+}
+
+
